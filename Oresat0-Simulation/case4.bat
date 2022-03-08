cc0cabef16775971c16a1624e495c8f09c5db56f
zacaroni <zgolden@pdx.edu>
2022-03-07 18:59:55 -0800

    My current version of the Oresat0 simulation. There may be files that can be deleted but I am not sure so I just uploaded the entire working directory

---
+++ Oresat0-Simulation/case4.bat
@@ -0,0 +1,6 @@
+SETLOCAL
+set path=C:\Program Files\Cullimore and Ring\Shared64;%PATH%
+set path=C:\Program Files\Cullimore and Ring\Thermal Desktop;%PATH%
+chdir "C:\Users\zachg\OneDrive\Documents\School\.Oresat0 Thermal Sim\"
+"C:\Program Files\Cullimore and Ring\Thermal Desktop\radcad_cl64.exe" "Exterior-Y-spin-RcOptics.rch" 1 Y 0
+"C:\Program Files\Cullimore and Ring\Thermal Desktop\radcad_cl64.exe" "Interior-Y-spin-RcOptics.rch" 1 Y 0
---
+++ Oresat0-Simulation/case5.bat
@@ -0,0 +1,6 @@
+SETLOCAL
+set path=C:\Program Files\Cullimore and Ring\Shared64;%PATH%
+set path=C:\Program Files\Cullimore and Ring\Thermal Desktop;%PATH%
+chdir "C:\Users\zachg\OneDrive\Documents\School\.Oresat0 Thermal Sim\"
+"C:\Program Files\Cullimore and Ring\Thermal Desktop\radcad_cl64.exe" "Exterior-x_spin_orbit-RcOptics.rch" 1 Y 0
+"C:\Program Files\Cullimore and Ring\Thermal Desktop\radcad_cl64.exe" "Interior-x_spin_orbit-RcOptics.rch" 1 Y 0
